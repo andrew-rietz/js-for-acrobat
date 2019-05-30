1ede5bc524bcff95d81a11c18a1bec84600ac4d7
Andrew Rietz <andrew.rietz@cadmusgroup.com>
2019-05-29 19:42:31 -0600

    Proper coords to adjust the BACK TO REPORT boxes

--- get_quads.js
+++ get_quads.js
@@ -157,3 +157,114 @@ var numWords = this.getPageNumWords(p);
 [BACK TO REPORT, 124, [323.26,246.49,443.58,224.29], link_to_page],
 [BACK TO REPORT, 126, [323.26,410.49,443.58,388.29], link_to_page],
 [BACK TO REPORT, 130, [323.26,255.49,443.58,233.29], link_to_page],
+
+
+
+
+
+
+
+
+
+
+***********************************************************************
+***********************************************************************
+***********************************************************************
+***********************************************************************
+
+
+
+
+
+
+
+for (var p = 3; p < this.numPages; p++){
+  if(p<34 || p>37){
+    var b = this.getPageBox("Crop", p);
+    this.removeLinks(p, b);
+  }
+}
+
+
+********************************
+********** PSE OUTPUT **********
+********************************
+["SEE THE DATA", 6, [417.26,493.49,521.58,477.29], link_to_page],
+["SEE THE DATA", 13, [1080.26,440.49,1190.58,418.29], link_to_page],
+["SEE THE DATA", 13, [1080.26,52.49,1190.58,30.29], link_to_page],
+["SEE THE DATA", 14, [468.26,443.49,578.58,421.29], link_to_page],
+["SEE THE DATA", 14, [468.26,77.49,578.58,55.29], link_to_page],
+["SEE THE DATA", 14, [1080.26,304.49,1190.58,282.29], link_to_page],
+["SEE THE DATA", 15, [1080.26,54.49,1190.58,32.29], link_to_page],
+["SEE THE DATA", 15, [1080.26,415.49,1190.58,393.29], link_to_page],
+["SEE THE DATA", 16, [1080.26,83.49,1190.58,61.29], link_to_page],
+["SEE THE DATA", 16, [1080.26,441.49,1190.58,419.29], link_to_page],
+["SEE THE DATA", 17, [468.26,396.49,579.58,373.29], link_to_page],
+["SEE THE DATA", 17, [468.26,60.49,579.58,37.29], link_to_page],
+["SEE THE DATA", 17, [1076.26,391.49,1186.58,368.29], link_to_page],
+["SEE THE DATA", 18, [1079.26,71.49,1189.58,48.29], link_to_page],
+["SEE THE DATA", 18, [1079.26,445.49,1189.58,423.29], link_to_page],
+["SEE THE DATA", 19, [1080.26,70.49,1190.58,47.29], link_to_page],
+["SEE THE DATA", 19, [1080.26,472.49,1190.58,450.29], link_to_page],
+["SEE THE DATA", 20, [1080.26,89.49,1190.58,67.29], link_to_page],
+["SEE THE DATA", 20, [1080.26,447.49,1190.58,424.29], link_to_page],
+["SEE THE DATA", 22, [1079.26,67.49,1189.58,45.29], link_to_page],
+["SEE THE DATA", 22, [1079.26,382.49,1189.58,360.29], link_to_page],
+["SEE THE DATA", 23, [468.26,443.49,579.58,421.29], link_to_page],
+["SEE THE DATA", 23, [468.26,49.49,579.58,27.29], link_to_page],
+["SEE THE DATA", 23, [1079.26,375.49,1189.58,353.29], link_to_page],
+["SEE THE DATA", 24, [1076.26,56.49,1186.58,33.29], link_to_page],
+["SEE THE DATA", 24, [1080.26,488.49,1190.58,466.29], link_to_page],
+["SEE THE DATA", 25, [1076.26,54.49,1186.58,32.29], link_to_page],
+["SEE THE DATA", 26, [467.26,65.49,577.58,43.29], link_to_page],
+["SEE THE DATA", 26, [468.26,436.49,579.58,414.29], link_to_page],
+["SEE THE DATA", 27, [1076.26,99.49,1186.58,77.29], link_to_page],
+["SEE THE DATA", 27, [1076.26,487.49,1186.58,465.29], link_to_page],
+["SEE THE DATA", 28, [468.26,55.49,579.58,32.29], link_to_page],
+["SEE THE DATA", 28, [468.26,420.49,579.58,398.29], link_to_page],
+["SEE THE DATA", 28, [1076.26,437.49,1186.58,414.29], link_to_page],
+["SEE THE DATA", 29, [1080.26,445.49,1191.58,423.29], link_to_page],
+["SEE THE DATA", 29, [1080.26,60.49,1191.58,38.29], link_to_page],
+["SEE THE DATA", 30, [1076.26,498.49,1186.58,476.29], link_to_page],
+["SEE THE DATA", 30, [1076.26,93.49,1186.58,70.29], link_to_page],
+["SEE THE DATA", 31, [467.26,592.49,577.58,569.29], link_to_page],
+["SEE THE DATA", 31, [468.26,163.49,578.58,140.29], link_to_page],
+["SEE THE DATA", 31, [468.26,398.49,578.58,376.29], link_to_page],
+["BACK TO REPORT", 39, [323.26,355.49,443.58,333.29], link_to_page],
+["BACK TO REPORT", 43, [323.26,422.49,443.58,400.29], link_to_page],
+["BACK TO REPORT", 43, [323.26,135.49,443.58,112.29], link_to_page],
+["BACK TO REPORT", 44, [323.26,313.49,443.58,291.29], link_to_page],
+["BACK TO REPORT", 53, [323.26,403.49,443.58,381.29], link_to_page],
+["BACK TO REPORT", 54, [323.26,245.49,443.58,223.29], link_to_page],
+["BACK TO REPORT", 58, [323.26,355.49,443.58,333.29], link_to_page],
+["BACK TO REPORT", 59, [323.26,339.49,443.58,317.29], link_to_page],
+["BACK TO REPORT", 59, [323.26,115.49,443.58,92.29], link_to_page],
+["BACK TO REPORT", 60, [323.26,344.49,443.58,322.29], link_to_page],
+["BACK TO REPORT", 60, [323.26,108.49,443.58,86.29], link_to_page],
+["BACK TO REPORT", 61, [323.26,286.49,443.58,263.29], link_to_page],
+["BACK TO REPORT", 63, [323.26,371.49,443.58,349.29], link_to_page],
+["BACK TO REPORT", 67, [323.26,266.49,443.58,243.29], link_to_page],
+["BACK TO REPORT", 69, [323.26,418.49,443.58,396.29], link_to_page],
+["BACK TO REPORT", 72, [323.26,107.49,443.58,85.29], link_to_page],
+["BACK TO REPORT", 73, [323.26,391.49,443.58,369.29], link_to_page],
+["BACK TO REPORT", 73, [323.26,139.49,443.58,117.29], link_to_page],
+["BACK TO REPORT", 76, [323.26,418.49,443.58,396.29], link_to_page],
+["BACK TO REPORT", 77, [323.26,418.49,443.58,396.29], link_to_page],
+["BACK TO REPORT", 78, [323.26,276.49,443.58,254.29], link_to_page],
+["BACK TO REPORT", 80, [323.26,348.49,443.58,325.29], link_to_page],
+["BACK TO REPORT", 82, [323.26,198.49,443.58,175.29], link_to_page],
+["BACK TO REPORT", 83, [323.26,339.49,443.58,317.29], link_to_page],
+["BACK TO REPORT", 84, [323.26,261.49,443.58,238.29], link_to_page],
+["BACK TO REPORT", 87, [323.26,423.49,443.58,401.29], link_to_page],
+["BACK TO REPORT", 88, [323.26,234.49,443.58,212.29], link_to_page],
+["BACK TO REPORT", 89, [323.26,135.49,443.58,112.29], link_to_page],
+["BACK TO REPORT", 90, [323.26,154.49,443.58,132.29], link_to_page],
+["BACK TO REPORT", 92, [323.26,434.49,443.58,412.29], link_to_page],
+["BACK TO REPORT", 94, [323.26,281.49,443.58,259.29], link_to_page],
+["BACK TO REPORT", 95, [323.26,423.49,443.58,401.29], link_to_page],
+["BACK TO REPORT", 95, [323.26,214.49,443.58,192.29], link_to_page],
+["BACK TO REPORT", 97, [323.26,407.49,443.58,385.29], link_to_page],
+["BACK TO REPORT", 98, [323.26,355.49,443.58,333.29], link_to_page],
+["BACK TO REPORT", 98, [323.26,99.49,443.58,77.29], link_to_page],
+["BACK TO REPORT", 100, [323.26,410.49,443.58,387.29], link_to_page],
+["BACK TO REPORT", 103, [323.26,418.49,443.58,396.29], link_to_page],
